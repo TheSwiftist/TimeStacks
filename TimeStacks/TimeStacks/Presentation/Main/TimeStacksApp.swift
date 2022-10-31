1011e85afa7d81e61229f1ecbb98c939f9eddb65
junbangg <wnstjr4620@naver.com>
2022-10-31 23:55:13 +0900

    Project Setup

---
+++ TimeStacks/TimeStacks/Presentation/Main/TimeStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStacksApp.swift
+//  TimeStacks
+//
+//  Created by Jun Bang on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct TimeStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
