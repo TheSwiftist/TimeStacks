4cc0c2d7e684e402ebbc07c4106318c34aa61ace
asong57 <1998thddk@gmail.com>
2022-11-22 23:15:34 +0900

    FEAT: 스택추가화면 기본 UI 추가 구현

--- TimeStacks/TimeStacks/Presentation/Plus/StackPlusView.swift
+++ TimeStacks/TimeStacks/Presentation/Plus/StackPlusView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct StackPlusView: View {
     @State private var title: String = ""
+    @State private var isTimerVersion = true
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var timerTime = Date()
     private let data = StackData()
     
     var body: some View {
         VStack {
             HStack {
-                Spacer()
-                    .frame(width: 30)
+                Spacer().frame(width: 30)
                 Button("취소", action: {
                 }).foregroundColor(.black)
                     .font(.system(size: 14).weight(.heavy))
@@ -24,33 +27,26 @@ struct StackPlusView: View {
                     .font(.system(size: 16).weight(.heavy))
                 Spacer()
                 Button("저장", action: {
-                    
                 }).foregroundColor(.black)
                     .font(.system(size: 14).weight(.heavy))
-                Spacer()
-                    .frame(width: 30)
+                Spacer().frame(width: 30)
             }
             
             Spacer().frame(height: 20)
             
-            HStack{
+            HStack {
                 Spacer()
                 Text("2022.11.13")
                     .font(.system(size: 11).weight(.heavy))
-                Spacer()
-                    .frame(width: 15)
+                Spacer().frame(width: 15)
             }
             
             Spacer().frame(height: 20)
             
-            HStack{
-                Spacer()
-                    .frame(width: 15)
+            HStack {
+                Spacer().frame(width: 15)
                 Text("Stack Title")
-                    .font(
-                        .system(size: 15)
-                            .weight(.heavy)
-                    )
+                    .font(.system(size: 15).weight(.heavy))
                 Spacer()
             }
             
@@ -63,33 +59,64 @@ struct StackPlusView: View {
             Spacer().frame(height: 20)
             
             HStack {
-                Spacer()
-                    .frame(width: 15)
+                Spacer().frame(width: 15)
                 Text("Timer")
-                    .font(
-                        .system(size: 15)
-                            .weight(.heavy)
-                    )
+                    .font(.system(size: 15).weight(.heavy))
                 Spacer().frame(width: 30)
                 Button("Timer ver.", action: {
-                }).font(
-                    .system(size: 13)
-                        .weight(.heavy)
-                ).foregroundColor(.green)
+                    isTimerVersion = true
+                }).font(.system(size: 13).weight(.heavy))
+                    .foregroundColor(.green)
                 Spacer().frame(width: 15)
                 Button("Clock ver.", action: {
-                }).font(
-                    .system(size: 13)
-                        .weight(.heavy)
-                ).foregroundColor(.pink)
-                Spacer()
+                    isTimerVersion = false
+                }).font(.system(size: 13).weight(.heavy))
+                    .foregroundColor(.pink)
                 Spacer()
             }
-
-            Spacer()
             
-            List(data.stacks) { stack in
-                StackRow(stack: stack)
+            Spacer().frame(height: 20)
+            
+            Group {
+                if isTimerVersion {
+                    HStack {
+                        Spacer().frame(width: 15)
+                        Text("Start")
+                            .font(.system(size: 13).weight(.heavy))
+                        Spacer()
+                    }
+                    
+                    DatePicker("", selection: $startDate, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    
+                    Spacer().frame(height: 20)
+                    
+                    HStack {
+                        Spacer().frame(width: 15)
+                        Text("End")
+                            .font(.system(size: 13).weight(.heavy))
+                        Spacer()
+                    }
+                    
+                    DatePicker("", selection: $endDate, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                } else {
+                    
+                }
+                
+                Spacer().frame(height: 20)
+                
+                HStack {
+                    Spacer()
+                    Button("완료", action: {
+                    }).font(.system(size: 14).weight(.heavy))
+                        .foregroundColor(.blue)
+                    Spacer().frame(width: 20)
+                }
+                
+                List(data.stacks) { stack in
+                    StackTableRow(stack: stack)
+                }
             }
         }
     }
@@ -102,29 +129,22 @@ struct StackData {
     ]
 }
 
-struct StackRow: View {
+struct StackTableRow: View {
     let stack: Stack
     
     var body: some View {
         HStack {
             Text(stack.title)
-                .font(
-                    .system(size: 13)
-                        .weight(.heavy)
-                ).foregroundColor(.pink)
+                .font(.system(size: 13).weight(.heavy))
+                .foregroundColor(.pink)
             Spacer()
             Text(" -> ")
-                .font(
-                    .system(size: 13)
-                        .weight(.heavy)
-                    
-                ).foregroundColor(.black)
+                .font(.system(size: 13).weight(.heavy))
+                .foregroundColor(.black)
             Spacer()
             Text("\(stack.startTime)")
-                .font(
-                    .system(size: 13)
-                        .weight(.heavy)
-                ).foregroundColor(.pink)
+                .font(.system(size: 13).weight(.heavy))
+                .foregroundColor(.pink)
         }
     }
 }
