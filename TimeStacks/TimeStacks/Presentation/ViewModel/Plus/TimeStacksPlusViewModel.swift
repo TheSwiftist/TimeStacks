5340b746691b7bd777c0f98883d3286adb78b5d2
asong57 <1998thddk@gmail.com>
2023-04-09 12:26:12 +0900

    FEAT: PlusView, ViewModel 분리

--- TimeStacks/TimeStacks/Presentation/ViewModel/Plus/TimeStacksPlusViewModel.swift
+++ TimeStacks/TimeStacks/Presentation/ViewModel/Plus/TimeStacksPlusViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by asong on 2023/01/28.
 //
 
-import Foundation
+import SwiftUI
+
+final class TimeStackPlusViewModel {
+    // MARK: - Properties - Internal
+    var stackTitle: String = ""
+    @Published var newStackItem: StackItem = StackItem()
+}
+
+// MARK: - Internal Methods
+
+extension TimeStackPlusViewModel: PlusViewModelType {
+    func calculateTopOffset(index: CGFloat) -> CGFloat {
+        return (index <= 2 ? index : 2) * 15
+    }
+    
+    func saveNewTimeStackItem() {
+        print("newStackItem : \(newStackItem.title)")
+    }
+}
